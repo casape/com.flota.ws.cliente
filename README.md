6c0124236fefa93ef64f1894d4fe3c420bc3ee63
Raxuk <raxuk8@gmail.com>
2017-11-15 12:29:00 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# com.flota.ws.cliente
---
+++ src/cliente/ClienteFlotaWS.java
@@ -0,0 +1,17 @@
+package cliente;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.*;
+
+public class ClienteFlotaWS {
+
+	// Sustituye esta clase por tu implementación.
+	// Deberías copiar y modificar ligeramente la clase cliente que has implementado por ejemplo en la solución con sockets o RMI
+	
+
+}
